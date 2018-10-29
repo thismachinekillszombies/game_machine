79fd8314ea16e560496b75bec18c379a62798da3
Richard Butterworth <zombies@richardbutterworth.co.uk>
2018-10-29 12:40:15 +0000

    Cleaned up errors in setup.py after uploading to pypi

--- setup.py
+++ setup.py
@@ -7,19 +7,20 @@ setup(
     name = 'game_machine',
     version = '0.1',
     description = 'A 2D sprite based game engine/PyGame wrapper',
-    long_description = long_description
+    url="https://github.com/thismachinekillszombies/game_machine",
+    long_description = long_description,
     long_description_content_type = "text/markdown", 
     author = 'Richard Butterworth',
     author_email = 'richard@richardbutterworth.co.uk',
     license = 'MIT',
     classifiers = [
         "Programming Language :: Python",
-        "Licence :: OSI Approved :: MIT License"
+        "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Topic :: Education",
         "Topic :: Games/Entertainment"
-        ]
+        ],
     packages = [ 'game_machine' ],
     install_requires = [
         'pygame',
